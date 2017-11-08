529739d493c94398cd96d9511d6c8c0bce480ed7
sangsik <toheart@gmail.com>
2017-11-08 15:07:06 +0900

    7.2.5

--- src/springbook/user/dao/UserDaoJdbc.java
+++ src/springbook/user/dao/UserDaoJdbc.java
@@ -3,6 +3,7 @@ package springbook.user.dao;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
 
 import javax.sql.DataSource;
 
@@ -11,60 +12,61 @@ import org.springframework.jdbc.core.RowMapper;
 
 import springbook.user.domain.Level;
 import springbook.user.domain.User;
+import springbook.user.sqlservice.SqlService;
 
 public class UserDaoJdbc implements UserDao {
-	public void setDataSource(DataSource dataSource) {
-		this.jdbcTemplate = new JdbcTemplate(dataSource);
-	}
-	
-	private JdbcTemplate jdbcTemplate;
-	
-	private RowMapper<User> userMapper = 
-		new RowMapper<User>() {
-				public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-				User user = new User();
-				user.setId(rs.getString("id"));
-				user.setName(rs.getString("name"));
-				user.setPassword(rs.getString("password"));
-				user.setEmail(rs.getString("email"));
-				user.setLevel(Level.valueOf(rs.getInt("level")));
-				user.setLogin(rs.getInt("login"));
-				user.setRecommend(rs.getInt("recommend"));
-				return user;
-			}
-		};
+    private JdbcTemplate jdbcTemplate;
+    private Map<String, String> sqlMap;
+    private SqlService sqlService;
 
-	public void add(User user) {
-		this.jdbcTemplate.update(
-				"insert into users(id, name, password, email, level, login, recommend) " +
-				"values(?,?,?,?,?,?,?)", 
-					user.getId(), user.getName(), user.getPassword(), user.getEmail(), 
-					user.getLevel().intValue(), user.getLogin(), user.getRecommend());
-	}
+    public void setSqlMap(Map<String, String> sqlMap) {
+        this.sqlMap = sqlMap;
+    }
 
-	public User get(String id) {
-		return this.jdbcTemplate.queryForObject("select * from users where id = ?",
-				new Object[] {id}, this.userMapper);
-	} 
+    public void setDataSource(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
 
-	public void deleteAll() {
-		this.jdbcTemplate.update("delete from users");
-	}
+    public void setSqlService(SqlService sqlService) {
+        this.sqlService = sqlService;
+    }
 
-	public int getCount() {
-		return this.jdbcTemplate.queryForInt("select count(*) from users");
-	}
+    private RowMapper<User> userMapper = new RowMapper<User>() {
+        public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+            User user = new User();
+            user.setId(rs.getString("id"));
+            user.setName(rs.getString("name"));
+            user.setPassword(rs.getString("password"));
+            user.setEmail(rs.getString("email"));
+            user.setLevel(Level.valueOf(rs.getInt("level")));
+            user.setLogin(rs.getInt("login"));
+            user.setRecommend(rs.getInt("recommend"));
+            return user;
+        }
+    };
 
-	public List<User> getAll() {
-		return this.jdbcTemplate.query("select * from users order by id",this.userMapper);
-	}
+    public void add(User user) {
+        this.jdbcTemplate.update(this.sqlService.getSql("userAdd"), user.getId(), user.getName(), user.getPassword(), user.getEmail(),
+                user.getLevel().intValue(), user.getLogin(), user.getRecommend());
+    }
 
-	public void update(User user) {
-		this.jdbcTemplate.update(
-				"update users set name = ?, password = ?, email = ?, level = ?, login = ?, " +
-				"recommend = ? where id = ? ", user.getName(), user.getPassword(), user.getEmail(), 
-		user.getLevel().intValue(), user.getLogin(), user.getRecommend(),
-		user.getId());
-		
-	}
+    public User get(String id) {
+        return this.jdbcTemplate.queryForObject(this.sqlService.getSql("userGet"), new Object[] { id }, this.userMapper);
+    }
+
+    public void deleteAll() {
+        this.jdbcTemplate.update(this.sqlService.getSql("userDeleteAll"));
+    }
+
+    public int getCount() {
+        return this.jdbcTemplate.queryForInt(this.sqlService.getSql("userGetCount"));
+    }
+
+    public List<User> getAll() {
+        return this.jdbcTemplate.query(this.sqlService.getSql("userGetAll"), this.userMapper);
+    }
+
+    public void update(User user) {
+        this.jdbcTemplate.update(this.sqlService.getSql("userUpdate"), user.getName(), user.getPassword(), user.getEmail(), user.getLevel().intValue(), user.getLogin(), user.getRecommend(), user.getId());
+    }
 }
---
+++ src/springbook/user/sqlservice/SqlRetrievalFailureException.java
@@ -0,0 +1,21 @@
+package springbook.user.sqlservice;
+
+@SuppressWarnings("serial")
+public class SqlRetrievalFailureException extends RuntimeException {
+
+    public SqlRetrievalFailureException() {
+        super();
+    }
+
+    public SqlRetrievalFailureException(String message) {
+        super(message);
+    }
+
+    public SqlRetrievalFailureException(Throwable cause) {
+        super(cause);
+    }
+
+    public SqlRetrievalFailureException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
